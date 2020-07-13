438a3b468d2e5e271eac6e6af8dc35edf390ceb5
ajmonda <ajmonda@gmail.com>
2020-07-13 19:12:31 -0400

    successful API render from input

--- app.js
+++ app.js
@@ -2,8 +2,12 @@
 
 const addressForm = document.querySelector('form')
 
-addressForm.addEventListener('submit', getAddress)
-addressForm.addEventListener('submit', testAPI)
+addressForm.addEventListener('submit', async (e) => {
+  const url = getAddress(e)
+  await testAPI(url)
+
+})
+
 
 const streetForm = document.querySelector('#street')
 const cityForm = document.querySelector('#city')
@@ -13,44 +17,57 @@ const stateForm = document.querySelector('#state')
 function getAddress(e) {
   e.preventDefault()
 
-  const address = `${streetForm.value}%20${streetForm.value}%20${stateForm.value}`
+  const address = `${streetForm.value}%20${cityForm.value}%20${stateForm.value}`
 
   const key = `AIzaSyC6r1AUum2tYX_mkkG_GNAJbbNlHq4s-ek`
   const url = `https://www.googleapis.com/civicinfo/v2/representatives?key=${key}&address=${address}`
 
   console.log(address)
-  console.log(url)
+  return url
 }
 
 async function testAPI(url) {
+
   try {
+
     const response = await axios.get(url)
 
-    const govtName = response.data.officials[4].name
-    const govtTitle = response.data.offices[4].name
-    const govtAddress = response.data.officials[4].address[0] // returns object
+    function renderSenator() {
 
-    console.log(govtName)
-    console.log(govtTitle)
-    console.log(govtAddress)
+      const senatorName = response.data.officials[2].name
+      const senatorTitle = response.data.offices[2].name
+      const senatorAddressObj = response.data.officials[2].address[0]
 
-    const h1 = document.createElement('h1')
-    const h2 = document.createElement('h2')
-    const h3 = document.createElement('h3')
+      const h1 = document.createElement('h1')
+      const h2 = document.createElement('h2')
+      const h3 = document.createElement('h3')
 
-    h1.innerText = govtName
-    h2.innerText = govtTitle
-    h3.innerText = govtAddress
+     
 
-    const body = document.querySelector('body')
+      h1.innerText = senatorName
+      h2.innerText = senatorTitle
+      
+       for (const key in senatorAddressObj) {
+        h3.append(`${senatorAddressObj[key]} `)
+       }
+      
+      const body = document.querySelector('body')
 
-    body.append(h1)
-    body.append(h2)
-    body.append(h3)
+      body.append(h1)
+      body.append(h2)
+      body.append(h3)
+
+    }
+
+    renderSenator()
+
+    console.log(response.data)
 
   } catch (err) {
     console.log(`error: ${err}`)
   } finally {
     console.log(`if successful, response displayed`)
+
   }
+
 }
