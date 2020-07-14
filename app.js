3cb915b88e6d8d7d3560fb3112213bf6ebd564d0
ajmonda <ajmonda@gmail.com>
2020-07-14 11:24:54 -0400

    js work, user navigation and basic formatting

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const addressForm = document.querySelector('form')
 
 addressForm.addEventListener('submit', async (e) => {
   const url = getAddress(e)
-  await testAPI(url)
+  await getAPI(url)
 })
 
 const streetForm = document.querySelector('#street')
@@ -13,6 +13,7 @@ const stateForm = document.querySelector('#state')
 
 
 function getAddress(e) {
+
   e.preventDefault()
 
   const address = `${streetForm.value}%20${cityForm.value}%20${stateForm.value}`
@@ -22,35 +23,89 @@ function getAddress(e) {
   return url
 }
 
-async function testAPI(url) {
+async function getAPI(url) {
 
   try {
     const response = await axios.get(url)
-    function renderSenator() {
+    function getGovt() {
+
+      // const form = document.getElementById('form')
 
       const senatorName = response.data.officials[2].name
       const senatorTitle = response.data.offices[2].name
       const senatorAddressObj = response.data.officials[2].address[0]
 
-      const h1 = document.createElement('h1')
+      // const h1 = document.createElement('h1')
+      // const h2 = document.createElement('h2')
+      // const h3 = document.createElement('h3')
+
+      // h1.append(senatorName)
+      // h2.append(senatorTitle)
+
+      // for (const key in senatorAddressObj) {
+      //   h3.append(`${senatorAddressObj[key]}`)
+      // }
+
+
+      // console.log(h1, h2, h3)
+      // const results = document.createElement('div')
+
+      // results.append(h1)
+      // results.append(h2)
+      // results.append(h3)
+
+      const form = document.querySelector('form')
+      form.remove()
+      document.querySelector('h1').innerText = "These topics need your voice."
+
+
+
+      
       const h2 = document.createElement('h2')
+      h2.innerText = 'The BREATHE Act'
       const h3 = document.createElement('h3')
+      h3.innerText = 'The BREATHE Act aims to divest federal resources from incarceration and policing.'
+      const button = document.createElement('button')
+      button.innerText = 'Select'
+      const a = document.createElement('a')
+      a.href = 'https://breatheact.org/'
+      a.innerText = 'Learn More'
 
-      h1.append(senatorName)
-      h2.append(senatorTitle)
 
-      for (const key in senatorAddressObj) {
-        h3.append(`${senatorAddressObj[key]}`)
+
+
+
+
+      const topics = document.createElement('div')
+      topics.id = 'topics'
+
+      const main = document.querySelector('main')
+      
+
+      topics.append(h2)
+      topics.append(h3)
+      topics.append(button)
+      topics.append(a)
+
+      main.append(topics)
+
+
+      writeLetter = (e) => {
+        e.preventDefault()
+        main.removeChild(topics)
+        document.querySelector('h1').innerText = 'That\'s it. Edit, sign, and copy your call to action below.'
       }
 
-      const body = document.querySelector('body')
 
-      body.append(h1)
-      body.append(h2)
-      body.append(h3)
+      button.addEventListener('click', (e) => {
+        writeLetter(e)
+      })
+
     }
 
-    renderSenator()
+    getGovt()
+
+
 
   } catch (err) {
     console.log(`error: ${err}`)
