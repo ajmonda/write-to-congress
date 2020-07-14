d5ddb99b12987106f215e332a7613f0ea8824e65
ajmonda <ajmonda@gmail.com>
2020-07-13 23:19:19 -0400

    initial ladning DOM content

--- app.js
+++ app.js
@@ -5,9 +5,7 @@ const addressForm = document.querySelector('form')
 addressForm.addEventListener('submit', async (e) => {
   const url = getAddress(e)
   await testAPI(url)
-
 })
-
 
 const streetForm = document.querySelector('#street')
 const cityForm = document.querySelector('#city')
@@ -18,20 +16,16 @@ function getAddress(e) {
   e.preventDefault()
 
   const address = `${streetForm.value}%20${cityForm.value}%20${stateForm.value}`
-
   const key = `AIzaSyC6r1AUum2tYX_mkkG_GNAJbbNlHq4s-ek`
   const url = `https://www.googleapis.com/civicinfo/v2/representatives?key=${key}&address=${address}`
 
-  console.log(address)
   return url
 }
 
 async function testAPI(url) {
 
   try {
-
     const response = await axios.get(url)
-
     function renderSenator() {
 
       const senatorName = response.data.officials[2].name
@@ -42,32 +36,25 @@ async function testAPI(url) {
       const h2 = document.createElement('h2')
       const h3 = document.createElement('h3')
 
-     
+      h1.append(senatorName)
+      h2.append(senatorTitle)
 
-      h1.innerText = senatorName
-      h2.innerText = senatorTitle
-      
-       for (const key in senatorAddressObj) {
-        h3.append(`${senatorAddressObj[key]} `)
-       }
-      
+      for (const key in senatorAddressObj) {
+        h3.append(`${senatorAddressObj[key]}`)
+      }
+
       const body = document.querySelector('body')
 
       body.append(h1)
       body.append(h2)
       body.append(h3)
-
     }
 
     renderSenator()
-
-    console.log(response.data)
 
   } catch (err) {
     console.log(`error: ${err}`)
   } finally {
     console.log(`if successful, response displayed`)
-
   }
-
 }
